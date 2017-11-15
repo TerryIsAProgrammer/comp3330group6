1f0f4b86dbee54f35304a39ce3324cbbd53ae9c6
TERRY\Terry <u3519936@connect.hku.hk>
2017-11-15 18:03:51 +0800

    now enter the main page can identify the user

--- COMP3330mobileapp/app/src/main/java/group6/comp3330mobileapp/HomePage.java
+++ COMP3330mobileapp/app/src/main/java/group6/comp3330mobileapp/HomePage.java
@@ -10,6 +10,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by Terry on 11/6/2017.
@@ -19,21 +27,51 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private DatabaseReference mDatabase;
+    TextView title ;
+    private int userKey;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
-        setNavigationViewListener();
 
+        setNavigationViewListener();
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close );
-
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        title = (TextView) findViewById(R.id.TitleView);
+
+        Bundle extras = getIntent().getExtras();
+        userKey = extras.getInt("userKey");
+
+        Query query = mDatabase.child("users").orderByChild("userID").equalTo(userKey);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot thisSnapshot: dataSnapshot.getChildren()) {
+                        String username = (String)thisSnapshot.child("username").getValue();
+                        String identity = (String)thisSnapshot.child("identity").getValue();
+                        title.setText("Username: " + username + " Identity:" + identity);
+                    }
+
+                }else{
+                    title.setText("Wrong query la dllm");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
     }
 
     @Override
